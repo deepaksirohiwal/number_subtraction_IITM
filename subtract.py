944547adf6b92c280345f8dd3bf85e5723a220a8
Deepak Sirohiwal <38135521+deepaksirohiwal@users.noreply.github.com>
2022-11-30 22:00:32 +0530

    text edit

--- subtract.py
+++ subtract.py
@@ -7,7 +7,7 @@ def subtract(dig1,dig2):
 
 number1 = st.number_input('Enter first number')
 number2=st.number_input('Enter second number')
-st.write('The differnece between number1 and number2 is ', subtract(number1,number2))
+st.write('The differnece between First and Second number is ', subtract(number1,number2))
 
 
 st.write('Created by Deepak Sirohiwal (21f1005692)')
